68a25c714712383dc130802861fbedf232933f2c
Sakib Hasan <sakib100.sa@gmail.com>
2024-02-12 23:49:11 +0600

    crating product schema

---
+++ server/src/models/productModal.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Product name is required"],
+        trim: true,
+        
+    },
+});
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
