48b3cfa1369e493e1abf85f8490a8f6b5f78e85b
Sakib Hasan <sakib100.sa@gmail.com>
2024-02-12 23:57:48 +0600

    creating product schema

--- server/src/models/productModal.js
+++ server/src/models/productModal.js
@@ -1,13 +1,29 @@
 const { Schema, model } = require("mongoose");
 
-const productSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "Product name is required"],
-        trim: true,
-        
+// name, slug, price, quantity, sold, shipping, description, productPictures, category, createdBy
+const productSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Product name is required"],
+            trim: true,
+            minLength: [2, "Product name must be at least 2 characters long"],
+            maxLength: [
+                150,
+                "Product name must be at most 150 characters long",
+            ],
+        },
+        slug: {
+            type: String,
+            required: [true, "Product slug is required"],
+            lowercase: true,
+            unique: true,
+        },
     },
-});
+    {
+        timestamps: true,
+    }
+);
 
 const Product = model("Product", productSchema);
 
