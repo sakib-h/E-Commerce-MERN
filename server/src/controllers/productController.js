65957707036ca5f86b43ae2f4fa2f0a0026f6c35
Sakib Hasan <sakib100.sa@gmail.com>
2024-02-19 04:02:27 +0600

    re-writing hanldeCreateProduct function

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,55 +1,23 @@
-const { successResponse } = require("./responseController");
 const createError = require("http-errors");
-const Product = require("../models/productModal");
-const slugify = require("slugify");
+const { successResponse } = require("./responseController");
+
 const handleCreateProduct = async (req, res, next) => {
     try {
         const { name, description, price, quantity, shipping, category } =
             req.body;
 
         const image = req.file;
-
-        if (!image) {
-            throw createError(400, "Image is required");
-        }
-
-        if (image.size > 1024 * 1024 * 5) {
-            throw createError(400, "Image should be less than 5mb");
-        }
-
-        const imageBufferString = image.buffer.toString("base64");
-
-        const isProductExists = await Product.exists({ name: name });
-        if (isProductExists) {
-            throw createError(409, "Product with this name already exists");
-        }
-
-        const product = await Product.create({
-            name: name,
-            slug: slugify(name),
-            description: description,
-            price: price,
-            quantity: quantity,
-            shipping: shipping,
-            image: imageBufferString,
-            category: category,
-        });
-
-        if (!product) {
-            throw createError(
-                500,
-                "Product creation failed. Please try again."
-            );
-        }
+        
 
         return successResponse(res, {
-            statusCode: 201,
+            statusCode: 200,
             message: "Product created successfully",
-            payload: { product },
         });
     } catch (error) {
         next(error);
     }
 };
 
-module.exports = { handleCreateProduct };
+module.exports = {
+    handleCreateProduct,
+};
