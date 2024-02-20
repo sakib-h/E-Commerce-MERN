55be462243e0521927691757f3e203a68a33aa15
Sakib Hasan <sakib100.sa@gmail.com>
2024-02-21 04:06:27 +0600

    validation added for existed products

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,10 +1,16 @@
 const createError = require("http-errors");
 const { successResponse } = require("./responseController");
+const Product = require("../models/productModal");
 
 const handleCreateProduct = async (req, res, next) => {
     try {
         const { name, description, price, quantity, shipping, category } =
             req.body;
+
+        const existedProduct = await Product.exists({ name: name });
+        if (existedProduct) {
+            throw createError(400, "Product name already exists");
+        }
 
         const image = req.file;
         if (!image) {
@@ -13,6 +19,8 @@ const handleCreateProduct = async (req, res, next) => {
         if (image.size > 1024 * 1024 * 2) {
             throw createError(400, "Product image should be less than 2 MB");
         }
+
+        const imageBufferString = image.buffer.toString("base64");
 
         return successResponse(res, {
             statusCode: 200,
